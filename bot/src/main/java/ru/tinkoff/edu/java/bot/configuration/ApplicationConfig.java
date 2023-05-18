b746901fbbc4078eeb620b076dbc85a2b70369ee
purplebape <g.kalentev23@gmail.com>
2023-05-18 13:34:27 +0500

    fixes

--- bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
@@ -3,6 +3,7 @@ package ru.tinkoff.edu.java.bot.configuration;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -28,7 +29,7 @@ public class ApplicationConfig {
     @Validated
     @Data
     public static class Bot {
-        @NotBlank
+        @Value("${bot.token}")
         private String token;
         @NotBlank
         private String name;
